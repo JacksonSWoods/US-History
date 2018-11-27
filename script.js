5c812d41109a65598cb4aaa233083bbfd26f396b
JacksonSWoods <45366657+JacksonSWoods@users.noreply.github.com>
2018-11-27 09:12:40 -0800

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,22 @@
 //calling the function in window.onload to make sure the HTML is loaded
 window.onload = function() {
+     var t;
      var pos = 0; 
     //our box element
     var box = document.getElementById('box');
     var container = document.getElementById('container');
-    var t = setInterval(move, 10);
-  
-    function move() {
-        if(pos >= container.offsetWidth - 50) {
+     window.onkeypress = function(event){
+    let key = event.key.toUpperCase();
+    if ( key == 'ARROWUP' ) {
+        alert();
+    } else if ( key == 'D' ) {
+        // 'D' key is pressed
+    }
+}
+          t = setInterval(moveLeft, 10);});
+
+    function moveLeft() {
+        if(pos >= container.offsetWidth + 50) {
             clearInterval(t);
         }
         else {
